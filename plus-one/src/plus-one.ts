f8a93a94429669c841b264c8df2c34c980d694e4
clark-lindsay <clarklindsay96@gmail.com>
2020-08-11 12:40:30 -0400

    plusOne will throw when given an empty input array

--- plus-one/src/plus-one.ts
+++ plus-one/src/plus-one.ts
@@ -1,3 +1,6 @@
 export function plusOne(intAsArray: number[]): number[] {
-	return intAsArray;
+	if (intAsArray.length === 0) {
+		throw new Error('the plusOne function requires a non-empty input');
+	}
+	return [1];
 }
--- plus-one/tests/plusOne.test.ts
+++ plus-one/tests/plusOne.test.ts
@@ -2,6 +2,10 @@ import {plusOne} from '../src/plus-one';
 
 describe('the plusOne function', () => {
 	it('returns [1] if the given array is [0]', () => {
-		expect(plusOne([0])).toBe([1]);
+		expect(plusOne([0])).toEqual([1]);
+	});
+
+	it('throws an error when it is given an empty array', () => {
+		expect(() => plusOne([])).toThrow();
 	});
 });
