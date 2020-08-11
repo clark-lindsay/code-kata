655feb0efd21c249b8ab63fdad230a67391d4047
clark-lindsay <clarklindsay96@gmail.com>
2020-08-11 17:49:06 -0400

    plus_one is correct for single digits

--- plus-one/rust-plus-one/src/lib.rs
+++ plus-one/rust-plus-one/src/lib.rs
@@ -9,12 +9,9 @@ pub fn plus_one(int_as_vector: Vec<i32>) -> Vec<i32> {
 mod tests {
 use super::*;
     #[test]
-    fn given_0() {
+    fn given_a_single_digit() {
         assert_eq!(plus_one(vec![0]), vec![1], "given [0], it returns [1]");
-    }
-
-    #[test]
-    fn given_3() {
         assert_eq!(plus_one(vec![3]), vec![4], "given [3], it returns [4]");
+        assert_eq!(plus_one(vec![8]), vec![9], "given [8], it returns [9]");
     }
 }
