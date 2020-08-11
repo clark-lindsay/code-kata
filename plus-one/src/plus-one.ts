7de3d79b0df414c66dc250288a4f5706f248966f
clark-lindsay <clarklindsay96@gmail.com>
2020-08-11 12:45:26 -0400

    plusOne can handle any single digit case

--- plus-one/src/plus-one.ts
+++ plus-one/src/plus-one.ts
@@ -2,5 +2,5 @@ export function plusOne(intAsArray: number[]): number[] {
 	if (intAsArray.length === 0) {
 		throw new Error('the plusOne function requires a non-empty input');
 	}
-	return [1];
+	return [intAsArray[0] + 1];
 }
--- plus-one/tests/plusOne.test.ts
+++ plus-one/tests/plusOne.test.ts
@@ -1,11 +1,13 @@
 import {plusOne} from '../src/plus-one';
 
 describe('the plusOne function', () => {
-	it('returns [1] if the given array is [0]', () => {
-		expect(plusOne([0])).toEqual([1]);
-	});
-
 	it('throws an error when it is given an empty array', () => {
 		expect(() => plusOne([])).toThrow();
+	});
+
+	it('returns the next number for any single-digit input', () => {
+		expect(plusOne([0])).toEqual([1]);
+		expect(plusOne([3])).toEqual([4]);
+		expect(plusOne([8])).toEqual([9]);
 	});
 });
