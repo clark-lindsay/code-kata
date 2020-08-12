08ec16e153f6bbefe8fa18bf8d374acc357cc4bf
clark-lindsay <clarklindsay96@gmail.com>
2020-08-12 08:54:20 -0400

    added extra tests for the vector to int conversion

--- plus-one/rust-plus-one/src/i32_vec_to_i32.rs
+++ plus-one/rust-plus-one/src/i32_vec_to_i32.rs
@@ -16,4 +16,17 @@ mod i32_vec_to_i32_tests {
     fn given_a_single_digit() {
         assert_eq!(transform(vec![1]), 1);
     }
+
+    #[test]
+    #[should_panic]
+    fn given_an_empty_vec() {
+        transform(vec![]);
+    }
+
+    #[test]
+    fn given_arbitrary_digit_sequence() {
+        assert_eq!(transform(vec![1, 9, 4, 5, 7]), 19457);
+        assert_eq!(transform(vec![4, 2, 9, 1]), 4291);
+        assert_eq!(transform(vec![2, 4, 7]), 247);
+    }
 }
