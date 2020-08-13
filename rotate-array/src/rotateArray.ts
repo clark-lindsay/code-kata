f176d92b87989b7357e58dea4b359d79833cc120
clark-lindsay <clarklindsay96@gmail.com>
2020-08-13 15:22:37 -0400

    Refactor: replace loop with pipeline

--- rotate-array/src/rotateArray.ts
+++ rotate-array/src/rotateArray.ts
@@ -1,8 +1,8 @@
 export function rotateArray(nums: number[], rotationOffset: number = 1): number[] {
 	const result = [...nums];
-	for (const [index, value] of nums.entries()) {
+	nums.forEach((value, index) => {
 		result[(index + rotationOffset) % result.length] = value;
-	}
+});
 
 	return result;
 }
