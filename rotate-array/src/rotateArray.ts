103e6ffee6fcc03bb19d407d2a4d60d6ace695b5
clark-lindsay <clarklindsay96@gmail.com>
2020-08-13 15:18:04 -0400

    can handle an arbitrary array of integers
    The time complexity for the solution is O(n),
    and the space complexity is O(n) also.
    I prefer this solution over an in place one since it is very
    easy to read, and has no side-effects, so it can easily fit into
    functional pipelines as well as minimize strange bugs.

--- rotate-array/src/rotateArray.ts
+++ rotate-array/src/rotateArray.ts
@@ -1,3 +1,8 @@
-export function rotateArray(nums: number[]): number[] {
-	return [];
+export function rotateArray(nums: number[], rotationOffset: number = 1): number[] {
+	const result = [...nums];
+	for (const [index, value] of nums.entries()) {
+		result[(index + rotationOffset) % result.length] = value;
+	}
+
+	return result;
 }
--- rotate-array/tests/rotateArray.test.ts
+++ rotate-array/tests/rotateArray.test.ts
@@ -4,4 +4,14 @@ describe('the rotateArray function', () => {
 	test('given an empty array. it returns an empty array', () => {
 		expect(rotateArray([])).toEqual([]);
 	});
+
+	test('given a single digit array, it should be returned identically', () => {
+		expect(rotateArray([1])).toEqual([1]);
+	});
+
+	test('given any array of nums, it should return the rotated array', () => {
+		expect(rotateArray([1, 2, 3], 2)).toEqual([2, 3, 1]);
+		expect(rotateArray([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([5,6,7,1,2,3,4]);
+		expect(rotateArray([-1,-100,3,99], 2)).toEqual([3,99,-1,-100]);
+	});
 });
