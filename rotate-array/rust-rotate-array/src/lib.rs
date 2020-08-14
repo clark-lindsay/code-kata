8ac7788a5951535817f08e5ec6d9b939e51f1750
clark-lindsay <clarklindsay96@gmail.com>
2020-08-14 09:18:14 -0400

    handle the simple case of a one element vector

--- rotate-array/rust-rotate-array/src/lib.rs
+++ rotate-array/rust-rotate-array/src/lib.rs
@@ -1,5 +1,5 @@
-fn rotate_vector(nums: Vec<i32>) -> Vec<i32> {
-    return vec![];
+fn rotate_vector(nums: Vec<i32>, rotationOffset: i32) -> Vec<i32> {
+    return nums;
 }
 
 #[cfg(test)]
@@ -8,6 +8,13 @@ mod tests {
 
     #[test]
     fn given_empty_vec() {
-        assert_eq!(rotate_vector(vec![]), vec![], "when given an empty vector, it returns an empty vector");
+        assert_eq!(rotate_vector(vec![], 1), vec![], "when given an empty vector, it returns an empty vector");
+    }
+
+    #[test]
+    fn given_single_element_vec() {
+        assert_eq!(rotate_vector(vec![1], 1), vec![1]);
+        assert_eq!(rotate_vector(vec![1], 5), vec![1]);
+        assert_eq!(rotate_vector(vec![1], 58), vec![1]);
     }
 }
