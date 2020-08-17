174fbb9b0f76265d891f13193ee94da254911bef
clark-lindsay <clarklindsay96@gmail.com>
2020-08-17 18:07:56 -0400

    it handles all cases that it claims to, and is well factored

--- first-unique-char/src/firstUniqueChar.ts
+++ first-unique-char/src/firstUniqueChar.ts
@@ -1,20 +1,37 @@
+interface CharInfo {
+	count: number
+	firstIndex: number
+}
+
 export function firstUniqueChar(str: string): number {
-	if (str.length === 0) return -1;
+	const charToInfo = getAllCharInfo(str);
 
-	const numberOfAppearances: { [id: string]: number } = {};
-	for (const char of str) {
-		if (numberOfAppearances[char]) {
-			numberOfAppearances[char] += 1;
+	for (const info of Object.values(charToInfo)) {
+		let result: CharInfo = {count: 0, firstIndex: str.length};
+
+		if (info.count === 1) {
+			if (info.firstIndex < result.firstIndex) {
+				result = info;
+			}
+		}
+
+		if (result.count === 1) {
+			return result.firstIndex;
+		}
+	}
+
+	return -1;
+}
+
+function getAllCharInfo(str: string): { [id: string]: CharInfo } {
+	const result: { [id: string]: CharInfo } = {};
+
+	for (const [index, char] of str.split('').entries()) {
+		if (result[char] !== undefined) {
+			result[char].count += 1;
 		} else {
-			numberOfAppearances[char] = 1;
+			result[char] = {count: 1, firstIndex: index};
 		}
 	}
-
-	for (const [char, count] of Object.entries(numberOfAppearances)) {
-		if (count === 1) {
-			return str.indexOf(char);
-		}
-
-		return -1;
-	}
+	return result;
 }
