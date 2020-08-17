3ddd04f1c2020b5053bddae26c6752aa7cc2ddd7
clark-lindsay <clarklindsay96@gmail.com>
2020-08-17 17:38:45 -0400

    can handle the simplest cases

--- first-unique-char/src/firstUniqueChar.ts
+++ first-unique-char/src/firstUniqueChar.ts
@@ -1,3 +1,20 @@
 export function firstUniqueChar(str: string): number {
-	return -1;
+	if (str.length === 0) return -1;
+
+	const numberOfAppearances: { [id: string]: number } = {};
+	for (const char of str) {
+		if (numberOfAppearances[char]) {
+			numberOfAppearances[char] += 1;
+		} else {
+			numberOfAppearances[char] = 1;
+		}
+	}
+
+	for (const [char, count] of Object.entries(numberOfAppearances)) {
+		if (count === 1) {
+			return str.indexOf(char);
+		}
+
+		return -1;
+	}
 }
--- first-unique-char/tests/firstUniqueChar.test.ts
+++ first-unique-char/tests/firstUniqueChar.test.ts
@@ -3,5 +3,14 @@ import {firstUniqueChar} from '../src/firstUniqueChar';
 describe('the firstUniqueChar function', () => {
 	test('given an empty string, it returns -1', () => {
 		expect(firstUniqueChar('')).toEqual(-1);
-	})
-})
+	});
+
+	test('given a string with all duplicate letters, it returns -1', () => {
+		expect(firstUniqueChar('anna')).toEqual(-1);
+		expect(firstUniqueChar('ANNA')).toEqual(-1);
+		expect(firstUniqueChar('latelate')).toEqual(-1);
+	});
+	test('given a string with one unique letter, it returns its index', () => {
+		expect(firstUniqueChar('beett')).toEqual(0);
+	});
+});
