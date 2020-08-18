0356c2bf12940da1b3cb52ac96fef94feb959942
clark-lindsay <clarklindsay96@gmail.com>
2020-08-18 10:43:58 -0400

    first stab at a working version, using Options and HashMap

--- first-unique-char/rust-first-unique-char/src/lib.rs
+++ first-unique-char/rust-first-unique-char/src/lib.rs
@@ -1,5 +1,29 @@
+use std::collections::HashMap;
+
+#[derive(Eq, PartialEq, Debug)]
+struct CharInfo {
+    first_index: usize,
+    count: i32
+}
+
 pub fn first_unique_char(text: &str) -> Option<usize> {
-    None
+    let mut chars = HashMap::new();
+    text.char_indices().for_each(| (index, char) | {
+        let info = chars.entry(char).or_insert(CharInfo { first_index: index, count: 0});
+        info.count += 1;
+    });
+
+    let mut result: Option<usize> = None;
+    chars.iter().for_each(| (_, info) | {
+        if info.count == 1 {
+            match result {
+                None => result = Some(info.first_index),
+                Some(x) => if info.first_index < x { result = Some(info.first_index) }
+            }
+        }
+    });
+    
+    return result;
 }
 
 #[cfg(test)]
@@ -23,4 +47,16 @@ mod tests {
             None => assert!(true)
         }
     }
+    #[test]
+    fn given_a_single_character_string() {
+        let result = first_unique_char("d");
+        match result {
+            Some(x) => {
+                println!("{}", x);
+                assert_eq!(x, 0);
+            },
+            None => panic!()
+        }
+    }
+
 }
